fe78249edb3442bde259e045adfbcd46849d3827
kss22 <79365696+kss22@users.noreply.github.com>
2021-02-20 16:41:15 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,11 @@
+import socket
+server = "127.0.0.1"
+port = 1450
+request_string = "127.0.0.2"
+receive_buffer_size = 4096
+mysocket = socket.socket()
+mysocket.connect(server, port)
+mysocket.send(request_string)
+response_string = mysocket.recv(receive_buffer_size)
+mysocket.close
+print(response_string)
